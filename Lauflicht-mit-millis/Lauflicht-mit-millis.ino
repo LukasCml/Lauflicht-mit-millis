c678ccbfb4adc2c5612b2bb86e15410063d36848
LukasCml <71316048+LukasCml@users.noreply.github.com>
2020-11-03 13:49:50 +0100

    Version 2

--- Lauflicht-mit-millis/Lauflicht-mit-millis.ino
+++ Lauflicht-mit-millis/Lauflicht-mit-millis.ino
@@ -1,14 +1,15 @@
 
-unsigned long intervall = 500;
-unsigned long startpunkt;
-unsigned long ende;
+/*
+  unsigned long intervall = 500;
+  unsigned long startpunkt;
+  unsigned long ende;
 
-int zaehler = -1;
+  int zaehler = -1;
 
-byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+  byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-byte ledZustand[8][8] =
-{
+  byte ledZustand[8][8] =
+  {
   {1, 0, 0, 0, 0, 0, 0, 0},
   {0, 1, 0, 0, 0, 0, 0, 0},
   {0, 0, 1, 0, 0, 0, 0, 0},
@@ -17,11 +18,11 @@ byte ledZustand[8][8] =
   {0, 0, 0, 0, 0, 1, 0, 0},
   {0, 0, 0, 0, 0, 0, 1, 0},
   {0, 0, 0, 0, 0, 0, 0, 1},
-};
+  };
 
 
-void setup()
-{
+  void setup()
+  {
   Serial.begin(9600);
   {
     for (byte i = 0; i < 9; i++)
@@ -29,10 +30,10 @@ void setup()
       pinMode (led[i], OUTPUT);
     }
   }
-}
+  }
 
-void loop()
-{
+  void loop()
+  {
   ende = millis();                          //aktuelle Zeitmarke setzen
   if ((ende - startpunkt) >= intervall)     //wenn größer/gleich 500ms gezählt wurden dann..
   {
@@ -48,44 +49,71 @@ void loop()
       }
     }
   }
-}
-
+  }
+*/
 
 
 /*
-        Einfaches Lauflicht mit 8 LEDs und millis anstatt delay
+  Einfaches Lauflicht mit 8 LEDs und millis anstatt delay
 
-        letzte Änderung:  15.10.2020
-        Version:          0.2
-        Hardware:         LEDs an Pin 2-9
-        Lösung von Mora Motta
+  letzte Änderung:  15.10.2020
+  Version:          0.2
+  Hardware:         LEDs an Pin 2 - 9
+  Lösung von Mora Motta
+*/
+
+#include <OneButton.h>
+OneButton button(10, false);
 
 
 int ledPins[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 unsigned long letzteZeit;
 int ledEinschaltDauer = 300;
-int aktuelleLED = 0;  //Start Led
+int aktuelleLED = 0;                                     //Start Led
+bool statusPower = false;
 
 void setup()
 {
   for (int i = 0; i < 8; i++)
   {
-    pinMode(ledPins[i], OUTPUT);    //Alle Pins auf Ausgang
+    pinMode(ledPins[i], OUTPUT);                        //Alle Pins auf Ausgang
   }
+  button.attachClick(StatusPowerSetzen);
 }
 
 void loop()
 {
-  if ((millis() - letzteZeit) > ledEinschaltDauer)   //Zeit abgelaufen?
+  button.tick();
+  if (statusPower == true)
   {
-    digitalWrite(ledPins[aktuelleLED], LOW);
-    aktuelleLED++;
-    if (aktuelleLED == 8)  //Falls der Endwert erreicht worden ist
+    if ((millis() - letzteZeit) > ledEinschaltDauer)   //Zeit abgelaufen?
     {
-      aktuelleLED = 0;     //auf Startwert stellen
+      if (aktuelleLED > -1)
+      {
+        digitalWrite(ledPins[aktuelleLED], LOW);
+      }
+      aktuelleLED++;
+      if (aktuelleLED == 8)                            //Falls der Endwert erreicht worden ist
+      {
+        aktuelleLED = 0;                               //auf Startwert stellen
+      }
+      digitalWrite(ledPins[aktuelleLED], HIGH);
+      letzteZeit = millis();
     }
-    digitalWrite(ledPins[aktuelleLED], HIGH);
-    letzteZeit = millis();
   }
+  else
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      digitalWrite(ledPins[i], LOW);
+    }
+    aktuelleLED = -1;
+  }
+  delay(10);
 }
-*/
+
+void StatusPowerSetzen()
+{
+  statusPower = !statusPower;
+}
+
