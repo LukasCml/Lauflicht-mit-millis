fbf9a7858451a4f770cb10a5dbbd2d5e5f87b16d
LukasCml <lukas.camehl@gmx.de>
2020-11-06 19:51:18 +0100

    Version 2

--- Lauflicht-mit-millis/Lauflicht-mit-millis.ino
+++ Lauflicht-mit-millis/Lauflicht-mit-millis.ino
@@ -1,65 +1,10 @@
 
 /*
-  unsigned long intervall = 500;
-  unsigned long startpunkt;
-  unsigned long ende;
-
-  int zaehler = -1;
-
-  byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-
-  byte ledZustand[8][8] =
-  {
-  {1, 0, 0, 0, 0, 0, 0, 0},
-  {0, 1, 0, 0, 0, 0, 0, 0},
-  {0, 0, 1, 0, 0, 0, 0, 0},
-  {0, 0, 0, 1, 0, 0, 0, 0},
-  {0, 0, 0, 0, 1, 0, 0, 0},
-  {0, 0, 0, 0, 0, 1, 0, 0},
-  {0, 0, 0, 0, 0, 0, 1, 0},
-  {0, 0, 0, 0, 0, 0, 0, 1},
-  };
-
-
-  void setup()
-  {
-  Serial.begin(9600);
-  {
-    for (byte i = 0; i < 9; i++)
-    {
-      pinMode (led[i], OUTPUT);
-    }
-  }
-  }
-
-  void loop()
-  {
-  ende = millis();                          //aktuelle Zeitmarke setzen
-  if ((ende - startpunkt) >= intervall)     //wenn größer/gleich 500ms gezählt wurden dann..
-  {
-    Serial.println(millis());
-    startpunkt = millis();                  //neuen Startzeitpunkt wählen
-    {
-      digitalWrite(led[zaehler], LOW);
-      digitalWrite(led[zaehler + 1], HIGH);
-      zaehler++;
-      if (zaehler == 8)
-      {
-        zaehler = -1;
-      }
-    }
-  }
-  }
-*/
-
-
-/*
   Einfaches Lauflicht mit 8 LEDs und millis anstatt delay
 
   letzte Änderung:  15.10.2020
   Version:          0.2
   Hardware:         LEDs an Pin 2 - 9
-  Lösung von Mora Motta
 */
 
 #include <OneButton.h>
@@ -116,4 +61,3 @@ void StatusPowerSetzen()
 {
   statusPower = !statusPower;
 }
-
